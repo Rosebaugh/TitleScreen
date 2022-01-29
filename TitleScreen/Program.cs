673c9867a8578bb9024eaecba4648fb6eae9580c
Caleb Rosebaugh <rosebaugh@ksu.edu>
2022-01-28 18:54:33 -0600

    Add project files.

---
+++ TitleScreen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TitleScreen
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
